0694368d3b9f85aa9a8389560c41a3969b7dce32
Klong <marvel13@hotmail.co.uk>
2022-03-30 11:24:49 +0100

    w2 d3 b - JS

--- front_end/javascript_folder/main.js
+++ front_end/javascript_folder/main.js
@@ -29,13 +29,61 @@ if (placeOfBirth === "Liverpool") {
 // || - or
 
 if (firstName=="Katherine" && lastName=="Long"){
-    console.log("My full name is Katherine Long")
+    console.log("My full name is Katherine Long");
 } else {
-    console.log("Who is this")
+    console.log("Who is this");
 }
 
 function myFunction() {
-    console.log("This is a JS function")
+    console.log("This is a JS function");
 }
 
 myFunction()
+
+// String interpolation - like an f string
+
+console.log(`My name is ${firstName} ${lastName}`);
+
+let genshinTeam=["Rosaria","Xingqiu","Fischl","Zhongli"];
+
+console.log(genshinTeam);
+console.log(genshinTeam[2]);
+
+// Loops
+let text=""
+for (let i=0; i<5; i++) {
+    text ="the number is " +i;
+    console.log(text)
+}
+
+let i=0
+while (i <10) {
+    text ="the number is " +i
+    console.log(text)
+    i++;
+}
+
+// Switch statements
+
+let fruit = "apple"
+switch (fruit) {
+    case "grape":
+        console.log("grape");
+        break;
+    case "orange":
+        console.log("orange");
+        break;
+    case "apple":
+        console.log("apple");
+        break;
+    default:
+        console.log("No.")
+}
+
+// Arrow Functions
+
+hello = () => {
+    console.log("Hello World!");
+}
+
+hello()
